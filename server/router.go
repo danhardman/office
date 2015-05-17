402083fd2d612fad968345417416014069aa34d5
Dan Hardman <dan.hardman@live.co.uk>
2015-05-17 16:44:54 +0100

    Update front end UI design

--- server/router.go
+++ server/router.go
@@ -13,5 +13,8 @@ func Router() {
 	r.HandleFunc("/", GetIndex).Methods("GET")
 	r.HandleFunc("/UpdateTemp", UpdateTemp).Methods("POST")
 
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css", http.FileServer(http.Dir("../static/css"))))
+	r.PathPrefix("/fonts/").Handler(http.StripPrefix("/fonts", http.FileServer(http.Dir("../static/fonts"))))
+
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
